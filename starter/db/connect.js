1efd8914507722e5205f457e188c71ec6e6bb4ad
Ankit Sahu <ankit.sahu.as250@gmail.com>
2021-11-28 18:59:48 +0530

    Update connect.js

--- starter/db/connect.js
+++ starter/db/connect.js
@@ -2,7 +2,7 @@ const mongoose  = require('mong+oose')
 
 
 const ConnectionString2 =
-'mongodb+srv://ankit:Mongo_ankit@877@nodeexpresprojects.ppv1c.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority'
+
 
 const connectDB =(url)=>{
 
